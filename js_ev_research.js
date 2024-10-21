36e3f921217088adc3ebfb93ab5f9f43edc623bb
jtdoherty <161648987+jtdoherty@users.noreply.github.com>
2024-10-21 13:39:11 -0400

    Update js_ev_research.js

--- js_ev_research.js
+++ js_ev_research.js
@@ -66,9 +66,9 @@ function populateTable(bets) {
             betBody.innerHTML += row; // Append the new row to the table body
 
             // Update the Last Found At information
-            const lastFoundAtElement = document.getElementById('lastFoundAt');
-            if (lastFoundAtElement) {
-                lastFoundAtElement.innerHTML = `<small>Last Found At: ${new Date(bet.last_found_at).toLocaleString()}</small>`;
+            const event_start_timeElement = document.getElementById('lastFoundAt');
+            if (event_start_timeElement) {
+                event_start_timeElement.innerHTML = `<small>Last Found At: ${new Date(bet.last_found_at).toLocaleString()}</small>`;
             }
         } else {
             console.warn("Missing participants for:", bet.market_name); // Log a warning if data is missing
