496760ff5b11e27bbb22dfb0cb4b2fd2663fa033
jtdoherty <161648987+jtdoherty@users.noreply.github.com>
2024-10-21 13:48:08 -0400

    Update js_ev_research.js

--- js_ev_research.js
+++ js_ev_research.js
@@ -65,11 +65,7 @@ function populateTable(bets) {
             `;
             betBody.innerHTML += row; // Append the new row to the table body
 
-            // Update the Last Found At information
-            const lastFoundAtElement = document.getElementById('lastFoundAt');
-            if (lastFoundAtElement) {
-                lastFoundAtElement.innerHTML = `<small>Last Found At: ${new Date(bet.last_found_at).toLocaleString()}</small>`; // Update the last found time
-            }
+           
         } else {
             console.warn("Missing participants for:", bet.market_name); // Log a warning if data is missing
         }
@@ -100,6 +96,11 @@ async function loadPreGameData() {
         console.error("Could not load arbitrage data:", error);
     }
 }
+ // Update the Last Found At information
+ const lastFoundAtElement = document.getElementById('lastFoundAt');
+ if (lastFoundAtElement) {
+     lastFoundAtElement.innerHTML = `<small>Last Found At: ${new Date(bet.last_found_at).toLocaleString()}</small>`;
+ }
 
 
 
